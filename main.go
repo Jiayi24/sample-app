19814a2bfc82ff57b8507adbcb631cbe6fff01e4
Jiayi24 <student-04-edea832db62f@qwiklabs.net>
2025-03-18 03:19:56 +0000

    Awesome Lab

--- main.go
+++ main.go
@@ -25,5 +25,18 @@ import (
 )
 
 func main() {
+<<<<<<< HEAD
+=======
+	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler)
+	http.HandleFunc("/red", redHandler)
+	http.HandleFunc("/red", redHandler)
+>>>>>>> 9f68e8d (Awesome Lab)
 	http.ListenAndServe(":8080", nil)
+func redHandler(w http.ResponseWriter, r *http.Request) { 
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100)) 
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src) 
+	w.Header().Set("Content-Type", "image/png") 
+	png.Encode(w, img) 
+}
 }
