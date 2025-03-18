4a67755bc356f0d3b05c81b0598e71aa8d45863b
Jiayi24 <jyan593@uwo.ca>
2025-03-17 23:29:29 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,4 @@
 func main() {
-<<<<<<< HEAD
-	http.ListenAndServe(":8080", nil)
-=======
     http.HandleFunc("/blue", blueHandler)
     http.HandleFunc("/red", redHandler)
     http.ListenAndServe(":8080", nil)
@@ -12,5 +9,4 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
     draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
     w.Header().Set("Content-Type", "image/png")
     png.Encode(w, img)
->>>>>>> 666ac30 (Update dev deployment to version v2.0 with redHandler)
 }
